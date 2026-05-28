1d9cd1d81d12ede4e0e86a003e9b94d2cc8a9fd9
ager001 <152964009+ager001@users.noreply.github.com>
2026-05-28 13:37:08 +0300

    Update URL configurations and add about page to blog views

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+
 def home(request):
     return HttpResponse("<h1>Welcome to the Blog Home Page!</h1>")
+
+def about(request):
+    return HttpResponse("<h1>About the Blog</h1><p>This is a simple blog application built with Django.</p>")
