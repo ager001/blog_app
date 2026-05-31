d4d281303b108ac01c1af8cef3a88baad5b139bb
ager001 <152964009+ager001@users.noreply.github.com>
2026-05-31 12:04:40 +0300

    Refactor admin and views to register Post model and remove hardcoded posts

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-
-posts = [
-    {
-        'author': 'John Doe',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'created_at': 'August 27, 2024'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'created_at': 'August 28, 2024'
-    }
-]
-
 def home(request):
     context = {
         'posts': Post.objects.all()
