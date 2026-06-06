1110a822cb93fb0027e74169f6f4b991db7af123
ager001 <152964009+ager001@users.noreply.github.com>
2026-06-06 11:35:13 +0300

    Add Publisher model and associate it with Book model

--- books/models.py
+++ books/models.py
@@ -1,10 +1,21 @@
 from django.db import models
 
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+    address = models.CharField(max_length=200)
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
     published_date = models.DateField()
     price = models.DecimalField(max_digits=13, decimal_places=2)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
     
     def __str__(self):
         return self.title
+
