a1d4e75dab2eaa2f9656c2529415c0a83fd80ed3
ager001 <152964009+ager001@users.noreply.github.com>
2026-06-06 11:54:08 +0300

    Add PublisherSerializer and enhance BookSerializer with publisher details

--- books/serializers.py
+++ books/serializers.py
@@ -1,12 +1,26 @@
 from rest_framework import serializers
-from .models import Book
+from .models import Book, Publisher
+
+
+
+class PublisherSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Publisher
+        fields = '__all__'
 
 
 class BookSerializer(serializers.ModelSerializer):
+    
+    publisher_detail = PublisherSerializer(source='publisher', read_only=True)
+    publisher = serializers.PrimaryKeyRelatedField(
+        queryset=Publisher.objects.all(),
+        write_only=True
+    )
 
     class Meta:
         model = Book
-        fields = ['id', 'title', 'author', 'price']
+        fields = ['id', 'title', 'author', 'price', 'publisher', 'publisher_detail']
 
     # ✅ Field-level validation (ONLY title)
     def validate_title(self, value):
@@ -22,8 +36,8 @@ class BookSerializer(serializers.ModelSerializer):
 
     # ✅ Object-level validation (multiple fields)
     def validate(self, attrs):
-        title = attrs.get('title')
-        author = attrs.get('author')
+        title = attrs.get('title', getattr(self.instance, 'title', None))
+        author = attrs.get('author', getattr(self.instance, 'author', None))
 
         if title == author:
             raise serializers.ValidationError(
