2326fcbf2ad8c3f942fa1be6f0b868979ffdc381
ager001 <152964009+ager001@users.noreply.github.com>
2026-06-07 09:59:51 +0300

    Refactor BookListAPIView to use GenericAPIView for improved queryset handling and serialization

--- books/views.py
+++ books/views.py
@@ -1,13 +1,22 @@
-from rest_framework.views import APIView
+from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 
 from .models import Book
 from .serializers import BookSerializer
 
-class BookListAPIView(APIView):
-        def get(self, request):
-            books = Book.objects.all()
-            serializer = BookSerializer(books, many=True)
-            return Response(serializer.data)
-        
-        
+
+class BookListAPIView(GenericAPIView):
+
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+    def get(self, request):
+
+        books = self.get_queryset()
+
+        serializer = self.get_serializer(
+            books,
+            many=True
+        )
+
+        return Response(serializer.data)
