3443259a6e077d8cdae10b81a538118874e7ff0e
ager001 <152964009+ager001@users.noreply.github.com>
2026-06-07 12:55:16 +0300

    Add recent and cheapest actions to BookViewSet for enhanced book retrieval options

--- books/views.py
+++ books/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import Book
 from .serializers import BookSerializer
 
@@ -17,3 +19,15 @@ class BookViewSet(ModelViewSet):
 
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    
+    @action(detail=False, methods=['get'])
+    def recent(self, request):
+        recent_books = Book.objects.order_by('-published_date')[:5]
+        serializer = self.get_serializer(recent_books, many=True)
+        return Response(serializer.data)    
+    
+    @action(detail=False, methods=['get'])
+    def cheapest(self, request, pk=None):
+        cheapest_book = Book.objects.order_by('price').first()
+        serializer = self.get_serializer(cheapest_book)
+        return Response(serializer.data)
