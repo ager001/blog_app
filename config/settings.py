d0bc326196d15d8ef627d39244060df3e03aeb00
ager001 <152964009+ager001@users.noreply.github.com>
2026-06-12 21:28:53 +0300

    Add default permission classes to REST framework settings for enhanced security

--- config/settings.py
+++ config/settings.py
@@ -135,5 +135,8 @@ LOGIN_URL = 'login'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
 }
